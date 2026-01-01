fbec4febb2fb50ad8fbf0feabe88873dbd6a8f6a
Sara Mahmodi <saramah63@gmail.com>
2026-01-01 13:48:25 +0200

    Add basic SEO metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+export const metadata = {
+  title: "Mindshift for Lifeshift | Multilingual Coaching",
+  description:
+    "Transform your mindset, transform your life. Multilingual coaching (English & Persian) for international professionals, expats, and ESL leaders.",
+};
+
 import "./globals.css";
 import { Inter } from "next/font/google";
 
